49781abb00dfc7b68ba41261919b135815d1b530
Mauricio Lopes <mlopeesz@gmail.com>
2022-07-29 15:15:00 -0300

    feat: interface IVehicle

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+const vehicleZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().min(1900).max(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number(),
+});
+
+export type IVehicle = z.infer<typeof vehicleZodSchema>;
+export { vehicleZodSchema };
