7dcd1161665756e5e12faffa135066261341e186
Mauricio Lopes <mlopeesz@gmail.com>
2022-07-29 17:56:14 -0300

    feat: classe Car / Model

---
+++ src/models/Car.ts
@@ -0,0 +1,21 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MongoModel from './MongoModel';
+
+const carMongooseSchema = new Schema<ICar>({
+  status: Boolean,
+  model: String,
+  year: Number,
+  color: String,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+class Car extends MongoModel<ICar> {
+  constructor(model = mongooseCreateModel('Car', carMongooseSchema)) {
+    super(model);
+  }
+}
+
+export default Car;
