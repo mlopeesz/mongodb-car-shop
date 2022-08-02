3d8db04fe3149765e5d83698fae99f4249ee2ce9
Mauricio Lopes <mlopeesz@gmail.com>
2022-08-02 11:13:49 -0300

    fix: mudanças gerais em interface IVehicle

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -2,10 +2,10 @@ import { z } from 'zod';
 
 const vehicleZodSchema = z.object({
   model: z.string().min(3),
-  year: z.number().min(1900).max(2022),
+  year: z.number().gte(1900).lte(2022),
   color: z.string().min(3),
   status: z.boolean().optional(),
-  buyValue: z.number(),
+  buyValue: z.number().int(),
 });
 
 export type IVehicle = z.infer<typeof vehicleZodSchema>;
