14b4e1503ec69835fa501e7643f02e51505da5b1
Mauricio Lopes <mlopeesz@gmail.com>
2022-08-02 11:14:43 -0300

    feat/fix: implementa função create e mudanças em função read

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -1,15 +1,29 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { ICar } from '../interfaces/ICar';
 import { IService } from '../interfaces/IService';
 
 class CarController {
   constructor(private _service: IService<ICar>) {
+    this.create = this.create.bind(this);
     this.read = this.read.bind(this);
   }
 
-  public async read(_req: Request, res: Response) {
-    const cars = await this._service.read();
-    return res.status(200).json(cars);
+  public async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const createdCar = await this._service.create(req.body);
+      return res.status(201).json(createdCar);
+    } catch (error) {
+      next(error);
+    } 
+  }
+
+  public async read(_req: Request, res: Response, next: NextFunction) {
+    try {
+      const cars = await this._service.read();
+      return res.status(200).json(cars);
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
--- src/services/Car.ts
+++ src/services/Car.ts
@@ -1,16 +1,25 @@
-import { ICar } from '../interfaces/ICar';
+import { ICar, carZodSchema } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
 import { IService } from '../interfaces/IService';
 
 class CarService implements IService<ICar> {
-  private _car: IModel<ICar>;
+  private _model: IModel<ICar>;
   
   constructor(model: IModel<ICar>) {
-    this._car = model;
+    this._model = model;
+  }
+
+  public async create(obj:ICar):Promise<ICar> {
+    const parsed = carZodSchema.safeParse(obj);
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+
+    return this._model.create(obj);
   }
 
   public async read() {
-    const cars = await this._car.read();
+    const cars = await this._model.read();
     return cars;
   }
 }
